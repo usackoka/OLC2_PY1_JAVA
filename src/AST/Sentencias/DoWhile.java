5da69d207b1964fc85290f4af09887c4857f2efe
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-02-16 19:18:49 -0600

    DoWhile

---
+++ src/AST/Sentencias/DoWhile.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package AST.Sentencias;
+
+import AST.Entorno;
+import AST.Expresion;
+import AST.Nodo;
+import AST.Sentencia;
+import java.util.LinkedList;
+
+public class DoWhile extends Sentencia{
+
+    Expresion condicion;
+    LinkedList<Nodo> instrucciones;
+    
+    public DoWhile(Expresion condicion, LinkedList<Nodo> instrucciones, int fila, int columna){
+        this.instrucciones = instrucciones;
+        this.condicion = condicion;
+        this.fila = fila;
+        this.columna = columna;
+    }
+    
+    @Override
+    public Object ejecutar(Entorno entorno) {
+        do{
+            for (Nodo nodo : this.instrucciones)
+            {
+                if (nodo instanceof Sentencia)
+                {
+                    Object val = ((Sentencia)nodo).ejecutar(new Entorno(entorno));
+                    if (val != null)
+                    {
+                        //pregunto si es un continue
+                        if (val.equals(Corte.TIPO_CORTE.CONTINUE))
+                        {
+                            break;
+                        }
+                        else if (val.equals(Corte.TIPO_CORTE.BREAK))
+                        {
+                            return null;
+                        }
+                        return val;
+                    }
+                }
+                else
+                {
+                    ((Expresion)nodo).getValor(entorno);
+                }
+            }
+        }
+        while (Boolean.parseBoolean(this.condicion.getValor(entorno).toString()));
+        
+        return null;
+    }
+
+}
