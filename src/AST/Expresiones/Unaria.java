bc63c75f1753e6094795ba36c957232c21fd8d81
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-02-16 18:12:23 -0600

    Unaria

--- src/AST/Expresiones/Unaria.java
+++ src/AST/Expresiones/Unaria.java
@@ -8,25 +8,72 @@ package AST.Expresiones;
 
 import AST.Entorno;
 import AST.Expresion;
+import Analyzer.Token;
 
 public class Unaria extends Expresion{
 
     Expresion unaria;
+    TIPO_OPERACION tipoOperacion;
     
-    public Unaria(Expresion unaria, int fila, int columna){
+    public Unaria(Expresion unaria, TIPO_OPERACION tipoOperacion, int fila, int columna){
         this.unaria = unaria;
         this.fila = fila;
         this.columna = columna;
+        this.tipoOperacion = tipoOperacion;
+    }
+    
+    public enum TIPO_OPERACION{
+        MENOS,NOT
     }
     
     @Override
     public Object getValor(Entorno entorno) {
-        return null;
+        Object def = 0;
+        Object tipo = unaria.getTipo(entorno);
+        switch (tipoOperacion) {
+            case MENOS:
+                if (tipo.equals(Expresion.TIPO_PRIMITIVO.INTEGER))
+                {
+                    return - Integer.parseInt(unaria.getValor(entorno).toString());
+                }
+                else if (tipo.equals(Expresion.TIPO_PRIMITIVO.DOUBLE))
+                {
+                    return - Double.parseDouble(unaria.getValor(entorno).toString());
+                }
+            case NOT:
+                if (tipo.equals(Expresion.TIPO_PRIMITIVO.BOOLEAN))
+                {
+                    return !Boolean.parseBoolean(unaria.getValor(entorno).toString());
+                }
+                def = false;
+        }
+        
+        entorno.addError(new Token("Unaria", "No soportado" + tipoOperacion + " con: "+tipo, fila, columna));
+        return def;
     }
 
     @Override
     public Object getTipo(Entorno entorno) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object tipo = unaria.getTipo(entorno);
+        switch (tipoOperacion) {
+            case MENOS:
+                if (tipo.equals(Expresion.TIPO_PRIMITIVO.INTEGER))
+                {
+                    return Expresion.TIPO_PRIMITIVO.INTEGER;
+                }
+                else if (tipo.equals(Expresion.TIPO_PRIMITIVO.DOUBLE))
+                {
+                    return Expresion.TIPO_PRIMITIVO.DOUBLE;
+                }
+            case NOT:
+                if (tipo.equals(Expresion.TIPO_PRIMITIVO.BOOLEAN))
+                {
+                    return Expresion.TIPO_PRIMITIVO.BOOLEAN;
+                }
+        }
+        
+        entorno.addError(new Token("Unaria-getTipo", "No soportado" + tipoOperacion + " con: "+tipo, fila, columna));
+        return Expresion.TIPO_PRIMITIVO.STRING;
     }
 
 }
