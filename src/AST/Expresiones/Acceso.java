03e387401345ea19ef932ab4dd14774ceb7064b3
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-02-22 14:59:21 -0600

    Accesos en la gramática

---
+++ src/AST/Expresiones/Acceso.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package AST.Expresiones;
+
+import AST.Entorno;
+import AST.Expresion;
+
+public class Acceso extends Expresion{
+
+    public Acceso(){
+    }
+    
+    @Override
+    public Object getValor(Entorno entorno) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
